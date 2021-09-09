e948c10bb4530ce63c180d9a26d7c3690ae0c986
Godfrey Kinyua Muchomba <gkinyua83@gmail.com>
2021-09-09 21:28:01 +0300

    Add project files.

---
+++ EmployeeLibrary/IEDGE.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmployeeLibrary
+{
+    public interface IEdge<TVertex> : IComparable<IEdge<TVertex>> where TVertex : IComparable<TVertex>
+    {
+        bool IsWeighted { get; }
+
+        
+        TVertex Source { get; set; }
+        
+        TVertex Destination { get; set; }
+
+        Int64 Weight { get; set; }
+    }
+
+}
